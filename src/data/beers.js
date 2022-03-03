bc8d6e2b5225a2c3d34d84f55056b2df15ae8d07
Chad Schulz <chad.awesome@gmail.com>
2022-03-02 22:29:05 -0800

    Remove note from beers data (#9)

--- src/data/beers.js
+++ src/data/beers.js
@@ -1,15 +1,15 @@
 module.exports = {
   BEERS: [
-    { id: 'calvin-and-hops', name: 'Calvin And Hops IPA', note: '' },
+    { id: 'calvin-and-hops', name: 'Calvin And Hops IPA' },
     { id: 'charmed-sea', name: 'Charmed Sea IPA', note: 'Hazy IPA' },
     { id: 'dry-tiny-ipa', name: 'Dry Tiny Horses', note: 'WC IPA' },
     { id: 'friendly-wave', name: 'Friendly Wave IPA', note: 'West Coast' },
-    { id: 'halcyon-pale', name: 'Halcyon Pale', note: '' },
+    { id: 'halcyon-pale', name: 'Halcyon Pale' },
     { id: 'honey-lager', name: 'Hawaiian Honey Lager', note: 'Collaboration with Lucky Envelope' },
     { id: 'nz-pilsner', name: 'New Zealand Pilsner', note: 'NZ Pilsner' },
-    { id: 'pilsner', name: 'Pilsner', note: '' },
+    { id: 'pilsner', name: 'Pilsner' },
     { id: 'soft-metal', name: 'Soft Metal', note: 'French-style Pilsner' },
-    { id: 'keller-bitter', name: 'KellerBitter', note: '' },
-    { id: 'bullet-train', name: 'Bullet Train Pale', note: '' },
+    { id: 'keller-bitter', name: 'KellerBitter' },
+    { id: 'bullet-train', name: 'Bullet Train Pale' },
   ],
 };
