8cab1fbc191c21286d5d76d2abc317bff2119951
Chad Schulz <chad.awesome@gmail.com>
2022-03-12 09:05:29 -0800

    Adds 2022-03-10 (#13)

--- src/data/beers.js
+++ src/data/beers.js
@@ -1,15 +1,16 @@
 module.exports = {
   BEERS: [
+    { id: 'bullet-train', name: 'Bullet Train Pale' },
     { id: 'calvin-and-hops', name: 'Calvin And Hops IPA' },
-    { id: 'charmed-sea', name: 'Charmed Sea IPA', note: 'Hazy IPA' },
+    { id: 'charmed-sea', name: 'Charmed Sea Hazy IPA', note: 'Hazy IPA' },
     { id: 'dry-tiny-ipa', name: 'Dry Tiny Horses', note: 'WC IPA' },
     { id: 'friendly-wave', name: 'Friendly Wave IPA', note: 'West Coast' },
     { id: 'halcyon-pale', name: 'Halcyon Pale' },
+    { id: 'hanami-hazy', name: 'Hanami Hazy IPA' },
     { id: 'honey-lager', name: 'Hawaiian Honey Lager', note: 'Collaboration with Lucky Envelope' },
-    { id: 'nz-pilsner', name: 'New Zealand Pilsner', note: 'NZ Pilsner' },
-    { id: 'pilsner', name: 'Pilsner' },
-    { id: 'soft-metal', name: 'Soft Metal', note: 'French-style Pilsner' },
     { id: 'keller-bitter', name: 'KellerBitter' },
-    { id: 'bullet-train', name: 'Bullet Train Pale' },
+    { id: 'nz-pilsner', name: 'New Zealand Pilsner', note: 'NZ Pilsner' },
+    { id: 'pilsner', name: 'Halcyon Pilsner' },
+    { id: 'soft-metal', name: 'Soft Metal', note: 'French-style Pilsner' },
   ],
 };
