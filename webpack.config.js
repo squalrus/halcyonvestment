074357a6ef31e6b2c69529fd608d22e0c29aae1c
Chad Schulz <chad.awesome@gmail.com>
2022-03-27 15:30:58 -0700

    Components: Add SCSS compilation, HTML, and opensource separation

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/js'),
   },
 };
