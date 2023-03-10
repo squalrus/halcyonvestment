992f19c740a7c6f197241bdd8bdd8b3f28a2aece
Chad Schulz <chad.awesome@gmail.com>
2023-03-09 21:52:49 -0800

    2023-02-25 (#55)

--- src/js/Data/MemberData.js
+++ src/js/Data/MemberData.js
@@ -155,5 +155,8 @@ export const MemberData = {
         { date: '2023-02-11', beer: 'pilsner' },
         { date: '2023-02-18', beer: 'high-altitude-spy-balloon' },
         { date: '2023-02-18', beer: 'pilsner' },
+        { date: '2023-02-21', beer: 'high-altitude-spy-balloon' },
+        { date: '2023-02-21', beer: 'mark-of-the-yeast' },
+        { date: '2023-02-25', beer: 'high-altitude-spy-balloon' },
     ],
 };
